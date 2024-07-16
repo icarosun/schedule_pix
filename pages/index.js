c691b5c046d57a356cfa3c0dc293e7f43fc74a5a
Ícaro Pereira <46682140+icarosun@users.noreply.github.com>
2024-07-16 01:26:22 +0000

    add: screen schedule

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
 function Home() {
-    return <h1>Hello World</h1>;
+    return (<>
+        <h1>Schedule Your Appointment</h1>
+        <label>Name: </label>
+        <input placeholder="Enter your name" />
+
+        <label>Email: </label>
+        <input type = "email" placeholder = "Enter your email"/>
+
+        <label>Date: </label>
+        <input type = "date" />
+
+        <label>Time: </label>
+        <input type = "time" />
+
+        <button>Schedule Appointment</button> 
+    </>);
 }
 
 export default Home;
