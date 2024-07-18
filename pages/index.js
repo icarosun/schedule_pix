cbb26963d4ba8429ab7d3af61df9616737423c88
Ícaro Pereira <46682140+icarosun@users.noreply.github.com>
2024-07-18 02:42:35 +0000

    add: scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,22 @@
 function Home() {
-    return (<>
-        <h1>Schedule Your Appointment</h1>
-        <label>Name: </label>
-        <input placeholder="Enter your name" />
+  return (
+    <>
+      <h1>Schedule Your Appointment</h1>
+      <label>Name: </label>
+      <input placeholder="Enter your name" />
 
-        <label>Email: </label>
-        <input type = "email" placeholder = "Enter your email"/>
+      <label>Email: </label>
+      <input type="email" placeholder="Enter your email" />
 
-        <label>Date: </label>
-        <input type = "date" />
+      <label>Date: </label>
+      <input type="date" />
 
-        <label>Time: </label>
-        <input type = "time" />
+      <label>Time: </label>
+      <input type="time" />
 
-        <button>Schedule Appointment</button> 
-    </>);
+      <button>Schedule Appointment</button>
+    </>
+  );
 }
 
 export default Home;
